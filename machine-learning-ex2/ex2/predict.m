5ed0aa3e3c4d7c3a47dbcf5c5d9e0b24fa0b8ee6
luongli <luongintel2009@gmail.com>
2015-08-09 15:59:23 +0700

    Add: code to predict the rate of admission base on learned parameters and scores

--- machine-learning-ex2/ex2/predict.m
+++ machine-learning-ex2/ex2/predict.m
@@ -14,9 +14,9 @@ p = zeros(m, 1);
 %               your learned logistic regression parameters. 
 %               You should set p to a vector of 0's and 1's
 %
-
-
-
+Gx = sigmoid(X*theta);
+g1 = find(Gx >= 0.5);
+p(g1, 1) = 1;
 
 
 
