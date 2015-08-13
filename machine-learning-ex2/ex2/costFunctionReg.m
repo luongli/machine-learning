156c6ee072413e8240cf8257961d1c25c27550e1
luongli <luongintel2009@gmail.com>
2015-08-13 23:32:27 +0700

    Add: add code to compute cost function and gradient with regularization to avoid overfitting

--- machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/ex2/costFunctionReg.m
@@ -9,19 +9,24 @@ m = length(y); % number of training examples
 
 % You need to return the following variables correctly 
 J = 0;
-grad = zeros(size(theta));
-
+grad = zeros(1, size(theta));
+warning ("off", "Octave:broadcast");
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the cost of a particular choice of theta.
 %               You should set J to the cost.
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
+hx = sigmoid(X*theta);
+n = length(theta);
 
+% Compute cost function
+J = 1/m * sum( -y.*log(hx) - (1-y).*log(1-hx)) + lambda/(2*m)*sum(theta([2:n]).^2);
 
-
-
+% Compute gradient of the code function
+grad(1) = 1/m * sum( (hx-y) .* X(:,1) );
+grad([2:n]) = 1/m * sum((hx-y).*X(:, [2:n])) .+ lambda/m * theta([2:n])';
 
 
 % =============================================================
-
+warning ("on", "Octave:broadcast");
 end
